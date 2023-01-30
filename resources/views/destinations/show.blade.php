1990b337d33e545b0890077646a6da8f2d14f0ca
Davi Leichsenring <davileichs@gmail.com>
2023-01-30 10:44:17 +0100

    add Google Api key

--- resources/views/destinations/show.blade.php
+++ resources/views/destinations/show.blade.php
@@ -19,7 +19,7 @@
 
     <div class="row">
 
-        <h4>Destination Overview</h4>
+        <h4>{{ $destination->name }}</h4>
 
         <div class="card">
             <div class="card-header">
@@ -88,7 +88,7 @@
 
                             <div col="col-12">
 
-                                        <iframe class="w-100 " src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d3902543.2003194243!2d-118.04220880485131!3d36.56083290513502!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x80be29b9f4abb783%3A0x4757dc6be1305318!2sInyo%20National%20Forest!5e0!3m2!1sen!2sin!4v1576668158879!5m2!1sen!2sin"  allowfullscreen=""></iframe>
+                                        <iframe class="w-100 " src="https://www.google.com/maps/embed/v1/place?key={{ env('GOOGLE_API_KEY') }}&q=Frankfurt,Germany"  allowfullscreen=""></iframe>
 
                             </div>
 
